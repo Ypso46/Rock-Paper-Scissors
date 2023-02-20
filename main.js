75474f1571a7eae8bca6f4b66fcec9efc4b9ed00
Greg <gd.temp@pm.me>
2023-02-20 21:34:31 +0100

    Add the userInput and the random guessing for the computer choice

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-console.log("hello world")
+let prompt = require('prompt-sync')()
+
+function getComputerChoice() {
+    let randomNumber = Math.floor(Math.random() * 2)
+    if (randomNumber == 0) {
+        console.log("Computer chose: Rock")
+    } else if (randomNumber == 1) {
+        console.log("Computer chose: Paper")
+    } else {
+        console.log("Computer chose: Scissors")
+    }
+}
+
+
+let userInput = prompt("Choose between Rock, Paper or Scissors")
+console.log("You chose: " + userInput)
+
+console.log(getComputerChoice())
+
