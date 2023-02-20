67528cad1b0990a040ee2099e460bddc65cf3f83
Greg <gd.temp@pm.me>
2023-02-20 22:03:02 +0100

    Entirely coded

--- main.js
+++ main.js
@@ -1,19 +1,41 @@
 let prompt = require('prompt-sync')()
 
+//function to get a random choise for the computer
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 2)
+    let randomNumber = Math.floor(Math.random() * 3)
+    let computerChoice = ""
     if (randomNumber == 0) {
-        console.log("Computer chose: Rock")
+        computerChoice = "Rock"
     } else if (randomNumber == 1) {
-        console.log("Computer chose: Paper")
+        computerChoice = "Paper"
     } else {
-        console.log("Computer chose: Scissors")
+        computerChoice = "Scissors"
     }
+    return computerChoice
 }
 
-
+//ask a user input
 let userInput = prompt("Choose between Rock, Paper or Scissors")
 console.log("You chose: " + userInput)
 
-console.log(getComputerChoice())
+let computerChoice = getComputerChoice()
+console.log("Computer choice: " + computerChoice)
+
+
+//making the core of the program
+if (computerChoice == "Rock" && userInput == "Paper") {
+    console.log("You won!")
+} else if (computerChoice == "Rock" && userInput == "Scissors") {
+    console.log("Computer won!") 
+} else if (computerChoice == "Paper" && userInput == "Rock") {
+    console.log("Computer won!") 
+} else if (computerChoice == "Paper" && userInput == "Scissors") {
+    console.log("You won!")
+} else if (computerChoice == "Scissors" && userInput == "Paper") {
+    console.log("Computer won!")
+} else if (computerChoice == "Scissors" && userInput == "Rock") {
+    console.log("You won!") 
+} else {
+    console.log("It's a draw!")
+}
 
