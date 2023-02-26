beee8872f27a0405319aaac5e94b1a8e314ec1dd
Greg <gd.temp@pm.me>
2023-02-26 15:32:45 +0100

    Everything seems to work correctly

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
 //function to get a random choice for the computer
 function getComputerChoice() {
     let randomNumber = Math.floor(Math.random() * 3);
-    let computerSelection = "";
+    let computerSelection = '';
     if (randomNumber == 0) {
-        computerSelection = "Rock";
+        computerSelection = 'Rock';
     } else if (randomNumber == 1) {
-        computerSelection = "Paper";
+        computerSelection = 'Paper';
     } else {
-        computerSelection = "Scissors";
+        computerSelection = 'Scissors';
     }
     return computerSelection;
 }
 
 function playRound(playerSelection) {
     if (playerSelection == 'rock') {
-        console.log('Player plays Rock');
+        playerSelection = 'Rock';
     } else if (playerSelection == 'paper') {
-        console.log('Players plays Paper');
+        playerSelection = 'Paper';
     } else if (playerSelection == 'scissors') {
-        console.log('Player plays Scissors'); 
+        playerSelection = 'Scissors';
     } else {
         console.log('Something went wrong, contact Stan!');
     }
@@ -26,14 +26,14 @@ function playRound(playerSelection) {
 }
 
 const buttons = document.querySelectorAll('button');
-const content = document.querySelector('.playerResult');
+const content = document.querySelector('.gameResult');
 const resultPlayer = document.createElement('div');
-resultPlayer.classList.add('resultPlayer');
+resultPlayer.classList.add('gamePlayer');
 content.appendChild(resultPlayer);
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        resultPlayer.textContent = 'Player plays: ' + button.id;
+        resultPlayer.textContent = 'Player plays: ' + button.value;
         
     });
 });
@@ -50,32 +50,34 @@ buttons.forEach((button) => {
 });
 
 
-//making the code to determine the winner
-function determineWinner(playerSelection, computerSelection) {
-    if (computerSelection == "Rock" && playerSelection == "Paper") {
-        return 'Player won';
-    } else if (computerSelection == "Rock" && playerSelection == "Scissors") {
-        return 'Computer won!'; 
-    } else if (computerSelection == "Paper" && playerSelection == "Rock") {
-        return 'Computer won!'; 
-    } else if (computerSelection == "Paper" && playerSelection == "Scissors") {
-        return 'You won!';
-    } else if (computerSelection == "Scissors" && playerSelection == "Paper") {
-        return 'Computer won!';
-    } else if (computerSelection == "Scissors" && playerSelection == "Rock") {
-        return 'You won!'; 
-    } else {
-        return "It's a draw!";
-    }
-}
-
 //display the results in a new div
-const endGame = document.querySelector('.playerResult');
+const endGame = document.querySelector('.gameResult');
 const endResult = document.createElement('div');
 endGame.appendChild(endResult);
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        endResult.textContent = determineWinner();
-    })
-})
+        let computerChoice = getComputerChoice();
+        resultComputer.textContent = 'Computer plays ' + computerChoice;
+        endResult.textContent = determineWinner(button.value, computerChoice);
+    });
+});
+
+//making the code to determine the winner
+function determineWinner(playerSelection, computerSelection) {
+    if (computerSelection == 'Rock' && playerSelection == 'Paper') {
+        return 'Player won';
+    } else if (computerSelection == 'Rock' && playerSelection == 'Scissors') {
+        return 'Computer won!'; 
+    } else if (computerSelection == 'Paper' && playerSelection == 'Rock') {
+        return 'Computer won!'; 
+    } else if (computerSelection == 'Paper' && playerSelection == 'Scissors') {
+        return 'You won!';
+    } else if (computerSelection == 'Scissors' && playerSelection == 'Paper') {
+        return 'Computer won!';
+    } else if (computerSelection == 'Scissors' && playerSelection == 'Rock') {
+        return 'You won!'; 
+    } else {
+        return "It's a draw!";
+    }
+}
