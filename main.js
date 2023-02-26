5713cfedb6ca47b78e9a2d71737a2aea601bc1a2
Greg <gd.temp@pm.me>
2023-02-26 09:36:41 +0100

    Just need to fix the last step which is to display the winner, the rest seems to be ok

--- main.js
+++ main.js
@@ -1,81 +1,81 @@
-//let prompt = require('prompt-sync')()
-
-//function to get a random choise for the computer
-/*
+//function to get a random choice for the computer
 function getComputerChoice() {
-    let randomNumber = Math.floor(Math.random() * 3)
-    let computerChoice = ""
+    let randomNumber = Math.floor(Math.random() * 3);
+    let computerSelection = "";
     if (randomNumber == 0) {
-        computerChoice = "Rock"
+        computerSelection = "Rock";
     } else if (randomNumber == 1) {
-        computerChoice = "Paper"
+        computerSelection = "Paper";
     } else {
-        computerChoice = "Scissors"
+        computerSelection = "Scissors";
     }
-    return computerChoice
+    return computerSelection;
 }
 
-//ask a user input
-let userInput = prompt("Choose between Rock, Paper or Scissors")
-console.log("You chose: " + userInput)
-
-let computerChoice = getComputerChoice()
-console.log("Computer choice: " + computerChoice)
-
-
-//making the core of the program
-if (computerChoice == "Rock" && userInput == "Paper") {
-    console.log("You won!")
-} else if (computerChoice == "Rock" && userInput == "Scissors") {
-    console.log("Computer won!") 
-} else if (computerChoice == "Paper" && userInput == "Rock") {
-    console.log("Computer won!") 
-} else if (computerChoice == "Paper" && userInput == "Scissors") {
-    console.log("You won!")
-} else if (computerChoice == "Scissors" && userInput == "Paper") {
-    console.log("Computer won!")
-} else if (computerChoice == "Scissors" && userInput == "Rock") {
-    console.log("You won!") 
-} else {s
-    console.log("It's a draw!")
-}
-*/
-
-
-function playRound(playerSelection, computerSelection) {
+function playRound(playerSelection) {
     if (playerSelection == 'rock') {
         console.log('Player plays Rock');
     } else if (playerSelection == 'paper') {
         console.log('Players plays Paper');
     } else if (playerSelection == 'scissors') {
-        console.log('Player plays Scissors') 
+        console.log('Player plays Scissors'); 
     } else {
         console.log('Something went wrong, contact Stan!');
     }
-    // if (computerSelection == 'Rock') {
-    //     console.log('Player plays Rock');
-    // } else if (computerSelection == 'Paper') {
-    //     console.log('Players plays Paper');
-    // } else if (computerSelection == 'Scissors') {
-    //     console.log('Player plays Scissors') 
-    // } else {
-    //     console.log('Something went wrong, contact Stan!');
-    // }
+    return playerSelection;
 }
 
 const buttons = document.querySelectorAll('button');
-
 const content = document.querySelector('.playerResult');
-
-const result = document.createElement('div');
-result.classList.add('result');
-
-content.appendChild(result);
+const resultPlayer = document.createElement('div');
+resultPlayer.classList.add('resultPlayer');
+content.appendChild(resultPlayer);
 
 buttons.forEach((button) => {
     button.addEventListener('click', () => {
-        result.textContent = 'Player plays: ' + button.id;
+        resultPlayer.textContent = 'Player plays: ' + button.id;
+        
+    });
+});
+
+const resultComputer = document.createElement('div');
+resultComputer.classList.add('resultComputer');
+content.appendChild(resultComputer);
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        resultComputer.textContent = 'Computer plays ' + getComputerChoice();
+        
     });
 });
 
 
+//making the code to determine the winner
+function determineWinner(playerSelection, computerSelection) {
+    if (computerSelection == "Rock" && playerSelection == "Paper") {
+        return 'Player won';
+    } else if (computerSelection == "Rock" && playerSelection == "Scissors") {
+        return 'Computer won!'; 
+    } else if (computerSelection == "Paper" && playerSelection == "Rock") {
+        return 'Computer won!'; 
+    } else if (computerSelection == "Paper" && playerSelection == "Scissors") {
+        return 'You won!';
+    } else if (computerSelection == "Scissors" && playerSelection == "Paper") {
+        return 'Computer won!';
+    } else if (computerSelection == "Scissors" && playerSelection == "Rock") {
+        return 'You won!'; 
+    } else {
+        return "It's a draw!";
+    }
+}
+
+//display the results in a new div
+const endGame = document.querySelector('.playerResult');
+const endResult = document.createElement('div');
+endGame.appendChild(endResult);
+
+buttons.forEach((button) => {
+    button.addEventListener('click', () => {
+        endResult.textContent = determineWinner();
+    })
+})
